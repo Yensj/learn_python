cba75c3e2858e9dec5dbdd587c1222e78e314eef
Artem A. Demin <Yensj@Artems-MacBook-Pro.local>
2017-03-18 15:13:44 +0300

    бот с логгером

--- bot.py
+++ bot.py
@@ -2,6 +2,10 @@
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+import logging
+logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
 
 def main():
     updater = Updater("337618195:AAFlOtkSrbEyf4xkYUpFt2kEHNZWHM1BV-Q")
